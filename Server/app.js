433c9ad9768ffd39f9b825bd4e490c6d4e8008ce
bikramgiri <asgbrg.github@gmail.com>
2026-03-26 17:34:15 +0545

    update app

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ app.get ("/", (req, res) => {
     res.send ("Hello World!");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen (PORT, () => {
     console.log (`Server is running on port ${PORT}`);
 });
